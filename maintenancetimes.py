e2e8f7f727198b75aef66e44a3874e8716389c62
EQS\sergiojesus <up201305552@fe.up.pt>
2018-05-17 12:41:03 +0100

    no message

--- maintenancetimes.py
+++ maintenancetimes.py
@@ -12,7 +12,7 @@ warnings.filterwarnings('ignore')
 def main_pipeline(path):
     r"""
     For a given maintenance oriented .csv file, with the column scheme given by specification , this function is
-    capable of dividing complete and incomplete times in two different pandas.DataFrame.
+    capable of dividing total, complete and incomplete times in three different pandas.DataFrame.
     :param path: path to the .csv file
     :return: Dataframes
     """
@@ -22,12 +22,16 @@ def main_pipeline(path):
 
     earliest_time = raw_data['datetime'].min()
     latest_time = raw_data['datetime'].max()
-    date_maint_lists()
 
-    incomp_times_frame = pd.DataFrame(data=np.array(incomp_times_list), columns=columns_component, index=rows_machine)
-    comp_times_frame = pd.DataFrame(data=np.array(comp_times_list), columns=columns_component, index=rows_machine)
+    dates, failures = date_maint_lists(raw_data, earliest_time, latest_time, rows_machine, columns_component)
 
-    return incomp_times_frame, comp_times_frame
+    total_difs, incomp_difs, comp_difs = calculate_time_difs(dates, failures)
+
+    total_times_frame = pd.DataFrame(data=np.array(total_difs), columns=columns_component, index=rows_machine)
+    incomp_times_frame = pd.DataFrame(data=np.array(incomp_difs), columns=columns_component, index=rows_machine)
+    comp_times_frame = pd.DataFrame(data=np.array(comp_difs), columns=columns_component, index=rows_machine)
+
+    return total_times_frame, incomp_times_frame, comp_times_frame
 
 # ----------------------------------- #
 #       col_row_identification        #
@@ -39,7 +43,7 @@ def col_row_identification(raw_data_frame):
     For a given Dataframe, returns the unique values of the wanted rows and columns
     :param raw_data_frame: 
     :return: rows and columns names
-    """"
+    """
 
     rows = np.unique(raw_data_frame['machineID'].values)
     columns = np.unique(raw_data_frame['comp'].values)
@@ -51,23 +55,34 @@ def col_row_identification(raw_data_frame):
 # ----------------------------------- #
 
 
-def date_maint_lists():
+def date_maint_lists(raw_data_frame, start_time, end_time, machines, components):
     r"""
-    For the dataframe, removes in list form the dates and type of work order, retaining machine ID and component
-    :return: two lists (dates and work order)
+    For the data frame, removes in list form the dates and type of work order, retaining machine ID and component
+    :param raw_data_frame: Initial read data frame
+    :param start_time: first registry of time in date column
+    :param end_time: last registry of time in date column
+    :param machines: list of machine ids
+    :param components: list of component ids
+    :return: two lists (dates and work orders types)
     """
 
     times_list = list()
     fails_list = list()
-    for machine in machineIDs:
+    for machine in machines:
         machine_fail_list = list()
         machine_list = list()
-        for comp in comps:
-            aux_list = [begin_time] + list(maint[maint['machineID'] == machine][maint['comp'] == comp]['datetime']) + [
-                end_time]
-            aux_fail_list = list(maint[maint['machineID'] == machine][maint['comp'] == comp]['IF_FAIL']) + [0]
+
+        for comp in components:
+            aux_list = [start_time] + \
+                       list(raw_data_frame[raw_data_frame['machineID'] == machine]
+                            [raw_data_frame['comp'] == comp]['datetime']) + \
+                       [end_time]
+            aux_fail_list = list(raw_data_frame[raw_data_frame['machineID'] == machine]
+                                 [raw_data_frame['comp'] == comp]['IF_FAIL']) + [0]
+
             machine_list.append(aux_list)
             machine_fail_list.append(aux_fail_list)
+
         times_list.append(machine_list)
         fails_list.append(machine_fail_list)
 
@@ -78,28 +93,42 @@ def date_maint_lists():
 # ----------------------------------- #
 
 
-def calculate_time_difs():
+def calculate_time_difs(times_list, fails_list):
     r"""
-
-    :return:
+    Calculates the time between two maintenance operations and divides it or not in three lists
+    :param times_list: list of maintenance operations dates
+    :param fails_list: list of maintenance operations type
+    :return: three lists, consisting of times between maintenance with indiscriminate type, corrective and preventive
     """
-
+    total_times_list = list()
     incomp_times_list = list()
     comp_times_list = list()
+
     for i, machine in enumerate(times_list):
+        total_machine_list = list()
         incomp_machine_list = list()
         comp_machine_list = list()
+
         for j, failure in enumerate(machine):
+            total_failure_list = list()
             incomp_failure_list = list()
             comp_failure_list = list()
+
             for index in range(len(failure) - 1):
+                total_failure_list.append((failure[index + 1] - failure[index]).days)
+
                 if fails_list[i][j][index] == 0:
                     incomp_failure_list.append((failure[index + 1] - failure[index]).days)
+
                 elif fails_list[i][j][index] == 1:
                     comp_failure_list.append((failure[index + 1] - failure[index]).days)
+
+            total_machine_list.append(total_failure_list)
             incomp_machine_list.append(incomp_failure_list)
             comp_machine_list.append(comp_failure_list)
+            
+        total_times_list.append(total_machine_list)
         incomp_times_list.append(incomp_machine_list)
         comp_times_list.append(comp_machine_list)
 
-    return
+    return total_times_list, incomp_times_list, comp_times_list
